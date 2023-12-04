d7cd8ba04ade8cc96dd190f2609d4ec2a63fcd72
Tiago Souza <tiagoeduardosouza@gmail.com>
2023-12-04 09:26:26 -0300

    Incializando Projeto

---
+++ src/test/java/com/sansasdeve/impact/ImpactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansasdeve.impact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
